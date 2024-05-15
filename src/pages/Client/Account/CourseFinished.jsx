dcee07d35ce56a7c1652da69880cdf433f7885bb
Bùi Chí Thanh <93498438+tahhnn@users.noreply.github.com>
2024-05-15 21:13:24 +0700

    refactor: addLink Course

--- src/pages/Client/Account/CourseFinished.jsx
+++ src/pages/Client/Account/CourseFinished.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { format } from 'date-fns';
 import { useGetAllCateQuery } from '@/providers/apis/cateApi';
+import { Link } from 'react-router-dom';
 
 const CourseFinished = ({ dataFinished }) => {
     const { data: dataCate } = useGetAllCateQuery();
@@ -12,20 +13,24 @@ const CourseFinished = ({ dataFinished }) => {
                     return course.course_id.cate_id === e._id;
                 });
                 return (
-                    <div className="flex flex-column p-4 border rounded drop-shadow-xl">
-                        <img width={200} className="mx-auto rounded" src={course.course_id.thumb} alt="ngu" />
-                        <div className="my-2">
-                            <div>
-                                <p className="text-lg font-bold">{course.course_id.name}</p>
-                                <p className="text-xs leading-[10px] font-thin">
-                                    Đã mua ngày: {format(course?.createdAt, 'dd/MM/yyyy')}
-                                </p>
-                            </div>
-                            <div className="flex justify-between mt-1">
-                                <p className="bg-gray-200 mt-2 px-3 py-1 text-uppercase font-bold">{cate_name?.code}</p>
+                    <Link to={`/detail/${course?.course_id._id}`}>
+                        <div className="flex flex-column p-4 border rounded drop-shadow-xl">
+                            <img width={200} className="mx-auto rounded" src={course.course_id.thumb} alt="ngu" />
+                            <div className="my-2">
+                                <div>
+                                    <p className="text-lg font-bold">{course.course_id.name}</p>
+                                    <p className="text-xs leading-[10px] font-thin">
+                                        Đã mua ngày: {format(course?.createdAt, 'dd/MM/yyyy')}
+                                    </p>
+                                </div>
+                                <div className="flex justify-between mt-1">
+                                    <p className="bg-gray-200 mt-2 px-3 py-1 text-uppercase font-bold">
+                                        {cate_name?.code}
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </Link>
                 );
             })}
         </div>
